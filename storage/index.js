f1dc6db3dd47083382c3bd80d9d65febfbfe5529
GashonHussein <ghussein212@gmail.com>
2022-04-13 21:32:50 -0700

    Update index.js

--- storage/index.js
+++ storage/index.js
@@ -1,51 +1,19 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+//main traverses a folder and uploads each file to the storage bucket
+// ---- NAMING CONVENTION ----
+// The name of the folder should be the classification of the photos it contains
+// e.g 0
+// assets are included in gitignore
+
+const path = require("path");
 const fs = require("fs");
+const { upload } = require("./upload");
 
-const { Storage } = require("@google-cloud/storage");
-const storage = new Storage();
-const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
-
-const images = require("./util/images.json");
-
-const randomHash = () => Math.random().toString(36).substring(7);
-
-//update images.json
-const updateJSON = (images) => {
-  const data = JSON.stringify(images, null, 2);
-
-  fs.writeFile("./util/images.json", data, (err) => {
-    if (err) throw err;
-    console.log("Data written to file");
+const main = (folder) => {
+  const fileNames = fs.readdirSync(path.resolve(__dirname, `assets/${folder}`));
+  fileNames.forEach((file) => {
+    upload(`./assets/${folder}/${file}`, folder);
   });
 };
 
-const handleUpload = (data) => {
-  const uri = `https://storage.googleapis.com/${process.env.GCLOUD_STORAGE_BUCKET}/${data.imgRef}`;
-
-  images[`classification_${data.folder}`][data.imgRef] = { ...data, uri: uri };
-  updateJSON(images);
-};
-
-const upload = (path, classification) => {
-  const options = {
-    destination: `${classification}/${randomHash()}.png`,
-    public: true,
-    resumable: true,
-    metadata: {
-      metadata: {
-        classification: `${classification}`,
-      },
-    },
-  };
-  bucket
-    .upload(path, options)
-    .then((res) =>
-      handleUpload({ folder: classification, imgRef: res[1].name, mediaRef: res[1].mediaLink })
-    )
-    .catch((err) => console.log("error", err));
-  return;
-};
-
-module.exports = { upload };
+// e.g
+main("0");
