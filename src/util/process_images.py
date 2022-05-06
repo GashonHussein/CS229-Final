0be188627a6a19fa297982b14f106c7f5e6d7b41
GashonHussein <87954127+GashonHussein@users.noreply.github.com>
2022-05-06 15:47:31 -0700

    stringify flattened features

--- src/util/process_images.py
+++ src/util/process_images.py
@@ -21,19 +21,83 @@ db = firestore.client()
 
 # jsonpickle.decode(data[query_name]) to decode
 def upload_array(query_name, arr, classification, data):
-    arr_str = np.array2string(arr)
+    arr_str = arr.tolist()
     data["{}".format(classification)][query_name] = arr_str
 
     # with open('./util/data.json', 'w') as outfile:
     #     json.dump(data, outfile)
-        
-    print(arr)
     print("uploading",query_name)
-    db.collection('{}'.format(classification)).add({
-        u'feature_1': arr_str,
-        u'url': query_name,
-    })
+    # db.collection('{}'.format(classification)).add({
+    #     u'feature_1': arr_str,
+    #     u'url': query_name,
+    # })
 
+# this function is called for each image in the database
+def process_image(image_url, classification, data):
+    # Read
+    stackColors = io.imread('single_color_test_image.png')
+    # Split
+    red_features_matrix = stackColors[:, :, 0]
+    green_features_matrix = stackColors[:, :, 1]
+    blue_features_matrix = stackColors[:, :, 2]
+    # For CNN Keep channels in place
+    cnn_features_stack = stackColors[:, :, :3]
+    # For other models (logistic regression) flatten features/channels
+    flattened_features = cnn_features_stack.reshape(1, -1)
+    upload_array(image_url, flattened_features[0], classification, data)
+    return flattened_features[0]
+
+    
+    # all_samples_stackColors = np.array([])
+    # for image_url in image_urls:
+    #     stackColors = io.imread('single_color_test_image.png')
+    #     RGB_channels_only = stackColors[:, :, :3]
+    #     all_samples_stackColors = np.append(all_samples_stackColors, RGB_channels_only)
+    # # Split
+    # # Shapes: (num_images, image_dim_h, image_dim_w)
+    # all_red_features_matrix = all_samples_stackColors[:, :, :, 0]
+    # all_green_features_matrix = all_samples_stackColors[:, :, :, 1]
+    # all_blue_features_matrix = all_samples_stackColors[:, :, :, 2]
+    # # For CNN Keep channels in place
+    # # Shape: (num_images, image_dim_h, image_dim_w, 3) -> Same as all_samples_stackColors
+    # all_cnn_features_stack = all_samples_stackColors
+    # # For other models (logistic regression) flatten features/channels
+    # # Shape: (num_images, image_dim_h * image_dim_w * 3)
+    # all_flattened_features = cnn_features_stack.reshape(np.shape(all_cnn_features_stack)[0], -1)
+    # print(all_flattened_features)
+
+#testing one image
+def test_one(data):
+    url = "https://storage.googleapis.com/cs-229-storage-bucket/drowsiness/raw/10/qhqk03.png"
+    stackColors = io.imread(url)
+    # Split
+    red_features_matrix = stackColors[:, :, 0]
+    green_features_matrix = stackColors[:, :, 1]
+    blue_features_matrix = stackColors[:, :, 2]
+    # For CNN Keep channels in place
+    cnn_features_stack = stackColors[:, :, :3]
+    # For other models (logistic regression) flatten features/channels
+    flattened_features = cnn_features_stack.reshape(1, -1)
+    upload_array(url, flattened_features[0], 0, data)
+    return flattened_features
+
+def main():
+    data = {"0": {}, "5": {}, "10": {}}
+    print(test_one(data))
+    # all_flattened_features_by_class = np.array([])
+    # for i in range(0, 11, 5):
+    #     res = get_images_url(i)
+    #     curr_flattened_features = np.array([])
+    #     for image in res:
+    #         image_features = process_image(image, i, data)
+    #         curr_flattened_features = np.append(curr_flattened_features, image_features)
+    #     all_flattened_features_by_class = np.append(all_flattened_features_by_class, curr_flattened_features)
+    with open('./util/data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+if __name__ == "__main__":
+    main()
 
 
 """
@@ -201,69 +265,3 @@ def logistic_regression_full(all_data, train_percent = 0.8):
     logistic_acc_metrics = accuracy_calc(predictions_Y_hat, Y_test.T)
 
 """
-
-# this function is called for each image in the database
-def process_image(image_url, classification, data):
-    # Read
-    stackColors = io.imread('single_color_test_image.png')
-    # Split
-    red_features_matrix = stackColors[:, :, 0]
-    green_features_matrix = stackColors[:, :, 1]
-    blue_features_matrix = stackColors[:, :, 2]
-    # For CNN Keep channels in place
-    cnn_features_stack = stackColors[:, :, :3]
-    # For other models (logistic regression) flatten features/channels
-    flattened_features = cnn_features_stack.reshape(1, -1)
-    upload_array(image_url, flattened_features[0], classification, data)
-    return flattened_features[0]
-    
-    # all_samples_stackColors = np.array([])
-    # for image_url in image_urls:
-    #     stackColors = io.imread('single_color_test_image.png')
-    #     RGB_channels_only = stackColors[:, :, :3]
-    #     all_samples_stackColors = np.append(all_samples_stackColors, RGB_channels_only)
-    # # Split
-    # # Shapes: (num_images, image_dim_h, image_dim_w)
-    # all_red_features_matrix = all_samples_stackColors[:, :, :, 0]
-    # all_green_features_matrix = all_samples_stackColors[:, :, :, 1]
-    # all_blue_features_matrix = all_samples_stackColors[:, :, :, 2]
-    # # For CNN Keep channels in place
-    # # Shape: (num_images, image_dim_h, image_dim_w, 3) -> Same as all_samples_stackColors
-    # all_cnn_features_stack = all_samples_stackColors
-    # # For other models (logistic regression) flatten features/channels
-    # # Shape: (num_images, image_dim_h * image_dim_w * 3)
-    # all_flattened_features = cnn_features_stack.reshape(np.shape(all_cnn_features_stack)[0], -1)
-    # print(all_flattened_features)
-
-#testing one image
-def test_one(data):
-    url = "https://storage.googleapis.com/cs-229-storage-bucket/drowsiness/raw/10/qhqk03.png"
-    stackColors = io.imread(url)
-    # Split
-    red_features_matrix = stackColors[:, :, 0]
-    green_features_matrix = stackColors[:, :, 1]
-    blue_features_matrix = stackColors[:, :, 2]
-    # For CNN Keep channels in place
-    cnn_features_stack = stackColors[:, :, :3]
-    # For other models (logistic regression) flatten features/channels
-    flattened_features = cnn_features_stack.reshape(1, -1)
-    upload_array(url, flattened_features[0], 0, data)
-    return flattened_features
-
-def main():
-    data = {"0": {}, "5": {}, "10": {}}
-    print(test_one(data))
-    # all_flattened_features_by_class = np.array([])
-    # for i in range(0, 11, 5):
-    #     res = get_images_url(i)
-    #     curr_flattened_features = np.array([])
-    #     for image in res:
-    #         image_features = process_image(image, i, data)
-    #         curr_flattened_features = np.append(curr_flattened_features, image_features)
-    #     all_flattened_features_by_class = np.append(all_flattened_features_by_class, curr_flattened_features)
-    with open('./util/data.json', 'w') as outfile:
-        json.dump(data, outfile)
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/util/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # db.collection('{}'.format(classification)).add({
+    #     u'feature_1': arr_str,
+    #     u'url': query_name,
+    # })
